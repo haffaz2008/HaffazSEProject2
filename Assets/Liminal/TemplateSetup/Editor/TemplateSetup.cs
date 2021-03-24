dedd6ad5d710589d3289365d70a0d555e5dd528e
haffaz2008 <55048275+haffaz2008@users.noreply.github.com>
2021-03-25 09:29:46 +1100

    Initial commit

---
+++ Assets/Liminal/TemplateSetup/Editor/TemplateSetup.cs
@@ -0,0 +1,32 @@
+﻿namespace Liminal.Editor.TemplateSetup
+{
+    using UnityEngine;
+    using UnityEditor;
+
+    [InitializeOnLoad]
+    public class TemplateSetup
+        : MonoBehaviour
+    {
+        public const string IsSetupKey = "TemplateSetupComplete";
+
+        static TemplateSetup()
+        {
+            if(EditorPrefs.GetBool(IsSetupKey, false))
+                return;
+
+            EditorApplication.update += Init;
+        }
+
+        [MenuItem("Liminal/Reset Setup Key")]
+        private static void ResetKey()
+        {
+            EditorPrefs.SetBool(IsSetupKey, false);
+        }
+
+        private static void Init()
+        {
+            EditorApplication.update -= Init;
+            TemplateSetupWindow.OpenWindow();
+        }
+    }
+}
